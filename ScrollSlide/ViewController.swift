0544fec719bc1875e4ab031bd0c47ec26dc2d98f
Lee Chul <formathead79@gmail.com>
2020-06-02 18:33:40 +0900

    1. Initialization

---
+++ ScrollSlide/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScrollSlide
+//
+//  Created by formathead on 2020/06/02.
+//  Copyright © 2020 formathead. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
