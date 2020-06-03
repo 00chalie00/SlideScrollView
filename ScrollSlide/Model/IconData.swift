92860327bc79325b3626aaf5246c7677094ff75b
Lee Chul <formathead79@gmail.com>
2020-06-03 19:38:30 +0900

    DataSource
    1. IconData
    2. IcontableViewController

---
+++ ScrollSlide/Model/IconData.swift
@@ -0,0 +1,51 @@
+//
+//  IconData.swift
+//  ScrollSlide
+//
+//  Created by formathead on 2020/06/03.
+//  Copyright © 2020 formathead. All rights reserved.
+//
+
+import UIKit
+
+var icons = [icon]()
+
+class icon {
+    var title: String
+    var description: String
+    var image: UIImage!
+    
+    init(title: String, description: String, image: UIImage) {
+        self.title = title
+        self.description = description
+        self.image = image
+    }
+    
+    func summerIcons() -> [icon] {
+        icons = [
+            icon(title: "Sun", description: "A beautiful day", image: UIImage(named: "summericons_01")!),
+            icon(title: "Beach Ball", description: "Fun in the sand", image: UIImage(named: "summericons_02")!),
+            icon(title: "Swim Trunks", description: "Time to go swimming", image: UIImage(named: "summericons_03")!),
+            icon(title: "Bikini", description: "Fun in the sun", image: UIImage(named: "summericons_04")!),
+            icon(title: "Sand Bucket and Shovel", description: "Castles in the sand", image: UIImage(named: "summericons_05")!)
+        ]
+        
+        return icons
+    }
+    
+    func winterIcon() -> [icon] {
+        
+        icons = [
+            icon(title: "Ornament", description: "Hang on your tree", image: UIImage(named: "icons_winter_01")!),
+            icon(title: "Candy Cane", description: "Mmm, tasty", image: UIImage(named: "icons_winter_02")!),
+            icon(title: "Snowman", description: "A very happy soul", image: UIImage(named: "icons_winter_03")!),
+            icon(title: "Penguin", description: "Mario's friend", image: UIImage(named: "icons_winter_04")!),
+            icon(title: "Santa Hat", description: "Found it in the chimney", image: UIImage(named: "icons_winter_05")!)
+        ]
+        
+        return icons
+    }
+    
+    
+}
+
