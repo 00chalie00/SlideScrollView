9698c2fd6e75eb05b2688934369050bbbdadb1d8
Lee Chul <formathead79@gmail.com>
2020-06-10 18:08:55 +0900

    SideVC
    setupScrollview

--- ScrollSlide/Controller/SideVC.swift
+++ ScrollSlide/Controller/SideVC.swift
@@ -27,8 +27,25 @@ class SideVC: UIViewController {
         
         super.init(nibName: nil, bundle: nil)
         
+        
     }
     
+    func setupScrollview() {
+        scrollVC = UIScrollView()
+        //AutoresizingMask는 정해진 제약으로 Size를 조절함(상세 사항은 PDF 참조)
+        //Code로 View를 생성 시 AutoresizingMask의 기본 값음 Ture, IB로 View 생성 시 기본값은 False
+        scrollVC.translatesAutoresizingMaskIntoConstraints = false
+        scrollVC.isPagingEnabled = true
+        //bounces: Scroll이 Edge에 도달해 Bounce되면 호출됨
+        scrollVC.bounces = false
+        view.addSubview(scrollVC)
+        
+    }
+    
+    func setupViewController() {
+        
+        
+    }
    
     
     
